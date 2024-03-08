3bc0bfc9a3d56c159df2c0827a956908b42770ca
Toyi Shimizu <99409348+ths20003@users.noreply.github.com>
2024-03-08 17:47:08 -0500

    Update perfect_number.cpp

--- e008/perfect_number.cpp
+++ e008/perfect_number.cpp
@@ -5,13 +5,16 @@
 #include "perfect_number.h"
 
 bool isPerfect(int num) {
-    std::vector<int> vect_repeats(5, num);
-    std::vector<int> divisors;
-    for (int i = 1; i <= num / 2; ++i) {
-        if (num % i == 0) {
-            divisors.push_back(i);
-        }
-    }
-    int sum = std::accumulate(divisors.begin(), divisors.end(), 0);
+    // Create a vector containing numbers from 1 to num/2
+    std::vector<int> divisors(num / 2);
+    std::iota(divisors.begin(), divisors.end(), 1);
+
+    // Use std::accumulate to sum the divisors
+    int sum = std::accumulate(divisors.begin(), divisors.end(), 0,
+        [num](int acc, int divisor) {
+            return acc + (num % divisor == 0 ? divisor : 0);
+        });
+
+    // Check if the sum equals the number
     return sum == num;
 }
